a10af21a221ae2fcb1d70d41673efdbda06603e8
Jinhua Chen <jinhua.chen@omec-instruments.com>
2019-09-03 14:42:06 +0800

    修改

--- ExtendViewIntoTitleBarDemo/ExtendViewIntoTitleBarDemo/TitleBar.xaml.cs
+++ ExtendViewIntoTitleBarDemo/ExtendViewIntoTitleBarDemo/TitleBar.xaml.cs
@@ -148,7 +148,7 @@ namespace ExtendViewIntoTitleBarDemo
                 //Color pressedbgColor = ((SolidColorBrush)Application.Current.Resources["SystemControlBackgroundListMediumBrush"]).Color;
                 //Color pressedfgColor = ((SolidColorBrush)Application.Current.Resources["SystemControlForegroundBaseHighBrush"]).Color;
 
-                Color fgColor = ((SolidColorBrush)Resources["ButtonBackgroundBrush"]).Color;
+                Color fgColor = ((SolidColorBrush)Resources["ButtonForegroundColor"]).Color;
                 Color inactivefgColor = ((SolidColorBrush)Resources["ButtonInactiveForegroundBrush"]).Color;
                 Color hoverbgColor = ((SolidColorBrush)Resources["ButtonHoverBackgroundBrush"]).Color;
                 Color hoverfgColor = ((SolidColorBrush)Resources["ButtonHoverForegroundBrush"]).Color;
